25ca268b13cec3517d874563cded1f4971a0a561
codekidwhocodes <87214137+codekidwhocodes@users.noreply.github.com>
2023-02-14 17:46:33 -0800

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,47 +1,163 @@
 const Engine = Matter.Engine;
-const Render = Matter.Render;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
-const Body = Matter.Body;
-const Composites = Matter.Composites;
-const Composite = Matter.Composite;
 
-let engine;
-let world;
-var ball;
-var blower;
-var blowerMouth;
-var button;
+var engine, world;
+var canvas;
+var palyer, playerBase, playerArcher;
+var playerArrows = [];
+var board1, board2;
+var numberOfArrows = 10;
+
+var score = 0;
+
+function preload() {
+  backgroundImg = loadImage("./assets/background.png");
+}
 
 function setup() {
-  var canvas = createCanvas(500, 500);
+  canvas = createCanvas(windowWidth, windowHeight);
 
   engine = Engine.create();
   world = engine.world;
 
-  ball = new Ball(width / 2 + 80, height / 2 - 80, 80, 80);
-  blower = new Blower(width / 2 - 50, height / 2 + 50, 150, 20);
-  blowerMouth = new BlowerMouth(width / 2 + 70, height / 2 + 20, 100, 90);
-  button = createButton("Click to Blow");
-  button.position(width / 2, height - 100);
-  button.class("blowButton");
+  playerBase = new PlayerBase(300, 500, 180, 150);
+  player = new Player(285, playerBase.body.position.y - 153, 50, 180);
+  playerArcher = new PlayerArcher(
+    340,
+    playerBase.body.position.y - 180,
+    120,
+    120
+  );
 
-  button.mousePressed(blow);
-
+  board1 = new Board(width - 300, 330, 50, 200);
+  board2 = new Board(width - 550, height - 300, 50, 200);
 }
 
 function draw() {
-  background(59);
+  background(backgroundImg);
+
   Engine.update(engine);
 
-  blower.show();
-  ball.show();
-  blowerMouth.show();
+  playerBase.display();
+  player.display();
+  playerArcher.display();
+
+  board1.display();
+  board2.display();
+
+  for (var i = 0; i < playerArrows.length; i++) {
+    if (playerArrows[i] !== undefined) {
+      playerArrows[i].display();
+
+      var board1Collision = Matter.SAT.collides(
+        board1.body,
+        playerArrows[i].body
+      );
+
+      var board2Collision = Matter.SAT.collides(
+        board2.body,
+        playerArrows[i].body
+      );
+
+      /*if (board1Collision || board2Collision) {
+        score += 5;
+      }*/
+
+      /*if (board1Collision.collided && board2Collision.collided) {
+        score += 5;
+      }*/
+
+      if (board1Collision.collided || board2Collision.collided) {
+        score += 5;
+      }
+
+      /*if (board1Collision.collided || board2Collision.collided) {
+        score = 5;
+      }*/
+
+      
+      var posX = playerArrows[i].body.position.x;
+      var posY = playerArrows[i].body.position.y;
+
+      if (posX > width || posY > height) {
+        if (!playerArrows[i].isRemoved) {
+          playerArrows[i].remove(i);
+        } else {
+          playerArrows[i].trajectory = [];
+        }
+      }
+    }
+  }
+
+  // Title
+  fill("#FFFF");
+  textAlign("center");
+  textSize(40);
+  text("EPIC ARCHERY", width / 2, 100);
+
+  // Score
+  fill("#FFFF");
+  textAlign("center");
+  textSize(30);
+  text("Score " + score, width - 200, 100);
+
+  // Arrow Count
+  fill("#FFFF");
+  textAlign("center");
+  textSize(30);
+  text("Remaining Arrows : " + numberOfArrows, 200, 100);
+
+
+  if (numberOfArrows == 0) {
+    gameOver();
+  }
+
 }
 
-function blow() {
+function keyPressed() {
+  if (keyCode === 32) {
+    if (numberOfArrows > 0) {
+      var posX = playerArcher.body.position.x;
+      var posY = playerArcher.body.position.y;
+      var angle = playerArcher.body.angle;
 
-  Matter.Body.applyForce(ball.body, {x:0, y:0}, {x:0, y:-0.05});
+      var arrow = new PlayerArrow(posX, posY, 100, 10, angle);
+
+      arrow.trajectory = [];
+      Matter.Body.setAngle(arrow.body, angle);
+      playerArrows.push(arrow);
+      numberOfArrows -= 1;
+    }
+  }
 }
+
+function keyReleased() {
+  if (keyCode === 32) {
+    if (playerArrows.length) {
+      var angle = playerArcher.body.angle;
+      playerArrows[playerArrows.length - 1].shoot(angle);
+    }
+  }
+}
+
+function gameOver() {
+  swal(
+    {
+      title: `Game Over!!!`,
+      text: "Thanks for playing!!",
+      imageUrl:
+        "https://raw.githubusercontent.com/vishalgaddam873/PiratesInvision/main/assets/board.png",
+      imageSize: "150x150",
+      confirmButtonText: "Play Again"
+    },
+    function(isConfirm) {
+      if (isConfirm) {
+        location.reload();
+      }
+    }
+  );
+}
+
 
